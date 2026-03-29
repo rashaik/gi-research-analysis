66c92ace66817d344f4bca981440b543560a34a5
rashaik <rashaik1997@gmail.com>
2026-03-29 16:47:26 -0400

    fix: update CORS for Railway deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -50,10 +50,14 @@ app = FastAPI(title="GI Research API", lifespan=lifespan)
 # =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000"
+  #  allow_origins=[
+  #      "http://localhost:5173",
+  #      "http://127.0.0.1:5173",
+  #      "http://localhost:3000"
+  #  ],
+    origins = [
+    "http://localhost:3000",
+    "https://gi-research-frontend-production.up.railway.app", # Add your Railway URL here
     ],
     allow_credentials=True,
     allow_methods=["*"],
